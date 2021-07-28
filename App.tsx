bf810aefb75b06050d8b25983c9d85ec9104e5bf
Nicolas Nathan Campos <camposnicolas45@gmail.com>
2021-07-28 11:02:49 -0300

    Struct with fireabse

--- App.tsx
+++ App.tsx
@@ -1,8 +1,28 @@
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+//firebase
+import firebase from 'firebase/app';
+import "firebase/database";
+import "firebase/auth";
+
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseSecrets);
+}
+
 export default function App() {
+  useEffect(() => {
+    firebase.auth().signInAnonymously().then(async () => {
+      //do something
+      console.log('||===FIREBASE LOGGED===||');
+    }).catch((error) => {
+      console.log(error.message);
+    });
+  }, []);
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
